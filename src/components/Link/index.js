ee6b47c15467a07cd0ac1ed57b67a3d53a270351
shsangha <sssangha@ucalgary.ca>
2019-08-21 16:54:38 -0600

    moved custom transition link logic into reusable functional component

---
+++ src/components/Link/index.js
@@ -0,0 +1,80 @@
+import React from "react"
+import TransitionLink from "gatsby-plugin-transition-link"
+import PropTypes from "prop-types"
+import { TimelineLite } from "gsap"
+
+const animateIn = (
+  node,
+  {
+    length,
+    state: {
+      from: { x, y },
+    },
+  }
+) => {
+  const tl = new TimelineLite()
+
+  tl.set(node, {
+    x,
+    y,
+  }).to(node, length, {
+    x: 0,
+    y: 0,
+  })
+}
+
+const animateOut = (
+  node,
+  {
+    length,
+    state: {
+      to: { x, y },
+    },
+  }
+) => {
+  const tl = new TimelineLite()
+
+  tl.to(node, length, {
+    x,
+    y,
+  })
+}
+
+const Link = ({ children, moveOutTo, moveInFrom, length, to, className }) => (
+  <TransitionLink
+    className={className}
+    entry={{
+      length,
+      state: { from: { x: moveInFrom.x, y: moveInFrom.y } },
+      trigger: ({ node, entry }) => {
+        animateIn(node, entry)
+      },
+    }}
+    exit={{
+      length,
+      state: { to: { x: moveOutTo.x, y: moveOutTo.y } },
+      trigger: ({ node, exit }) => {
+        animateOut(node, exit)
+      },
+    }}
+    to={to}
+  >
+    {children}
+  </TransitionLink>
+)
+
+Link.propTypes = {
+  moveInFrom: PropTypes.shape({
+    x: PropTypes.string,
+    y: PropTypes.string,
+  }),
+  moveOutTo: PropTypes.shape({
+    x: PropTypes.string,
+    y: PropTypes.string,
+  }),
+  length: PropTypes.number.isRequired,
+  to: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+}
+
+export default Link
