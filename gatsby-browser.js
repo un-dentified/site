ca8602f120398e6968bf1d158eb88c01e6df6ef2
shsangha <sssangha@ucalgary.ca>
2019-08-29 18:39:28 -0600

    added fix for vw/vh issue on mobile screens

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,9 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 import "./src/scss/base/base.scss"
+
+document.body.style.height = `${window.innerHeight}px`
+
+window.addEventListener("resize", () => {
+  document.body.style.height = `${window.innerHeight}px`
+})
