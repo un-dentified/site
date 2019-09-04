6c9eae7af9846c4ac0854de5b9f1635a5a298ea9
shsangha <sssangha@ucalgary.ca>
2019-09-03 19:32:19 -0600

    improved a11y by adding tabtrapping and moved animations into nested gsap timelines still taking advantage of FLIP just easier to animate the form in/out states that way

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,22 +1,48 @@
 import React, { Component, createRef } from "react"
 import styles from "./style.module.scss"
+import { TimelineLite, Power0 } from "gsap"
 
 export default class Contact extends Component {
   textRef = createRef()
-  btnRef = createRef()
   wrapperRef = createRef()
+  btnRef = createRef()
+  firstInputRef = createRef()
+  lastInputRef = createRef()
+  formRef = createRef()
 
   state = {
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
+    values: {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    },
+    errors: {
+      name: null,
+      email: null,
+      subject: null,
+      message: null,
+    },
+    touched: {
+      name: false,
+      email: false,
+      subject: false,
+      message: false,
+    },
+    visited: {
+      name: false,
+      email: false,
+      subject: false,
+      message: false,
+    },
     formOpen: false,
+    formVisible: false,
   }
 
   toggleForm = () => {
     this.setState(prevState => ({
       formOpen: !prevState.formOpen,
+      formVisible: true,
     }))
   }
 
@@ -25,18 +51,57 @@ export default class Contact extends Component {
       return [
         this.wrapperRef.current.getBoundingClientRect(),
         this.textRef.current.getBoundingClientRect(),
-        this.btnRef.current.getBoundingClientRect(),
       ]
     }
+    return null
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.focusTrap)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.focusTrap)
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (snapshot && prevState.formOpen !== this.state.formOpen) {
-      let current = [
-        this.wrapperRef.current,
-        this.textRef.current,
-        this.btnRef.current,
-      ]
+      const master = new TimelineLite()
+      master.addLabel("start")
+      let current = [this.wrapperRef.current, this.textRef.current]
+
+      if (this.state.formOpen) {
+        this.firstInputRef.current.focus()
+      }
+
+      if (this.state.formOpen) {
+        const tl = new TimelineLite()
+
+        tl.to(
+          this.formRef.current,
+          0.5,
+          {
+            opacity: 1,
+          },
+          "start+=0.5"
+        )
+        master.add(tl)
+      } else {
+        const tl = new TimelineLite()
+
+        tl.to(
+          this.formRef.current,
+          0.1,
+          {
+            opacity: 0,
+            onComplete: () => {
+              this.btnRef.current.focus()
+            },
+          },
+          "start"
+        )
+        master.add(tl)
+      }
 
       current.map((el, index) => {
         const elRect = el.getBoundingClientRect()
@@ -46,16 +111,53 @@ export default class Contact extends Component {
         const translateX = snapshot[index].left - elRect.left
         const translateY = snapshot[index].top - elRect.top
 
-        el.style.transition = ""
-        el.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scaleX},${scaleY})`
-      })
+        const tl = new TimelineLite()
 
-      requestAnimationFrame(() => {
-        current.map(el => {
-          el.style.transition = "all 2s"
-          el.style.transform = ""
-        })
+        tl.fromTo(
+          el,
+          0.5,
+          {
+            scaleX,
+            scaleY,
+            x: translateX,
+            y: translateY,
+          },
+          {
+            x: 0,
+            y: 0,
+            scaleX: 1,
+            scaleY: 1,
+            ease: Power0.easeOut,
+          }
+        )
+        master.add(tl, "start")
       })
+    }
+  }
+
+  focusTrap = event => {
+    if (event.keyCode === 9 && this.state.formVisible) {
+      if (
+        document.activeElement === this.lastInputRef.current &&
+        !event.shiftKey
+      ) {
+        event.preventDefault()
+
+        this.btnRef.current.focus()
+      }
+
+      if (document.activeElement === this.btnRef.current) {
+        if (!event.shiftKey) {
+          this.firstInputRef.current.focus()
+        } else {
+          this.lastInputRef.current.focus()
+        }
+        event.preventDefault()
+      }
+    }
+    if (event.keyCode === 27 && this.state.formVisible) {
+      this.btnRef.current.focus()
+      this.toggleForm()
     }
   }
 
@@ -63,11 +165,14 @@ export default class Contact extends Component {
     const { invert } = this.props
     const invertClass = invert ? styles["invert"] : ""
     const formOpenClass = this.state.formOpen ? `${styles.formOpen}` : ""
+    console.log(invertClass)
 
     return (
-      <div className={styles.wrapper}>
+      <div className={`${styles.wrapper} ${invertClass}`}>
         <button
-          className={`${styles.button} ${invertClass}`}
+          tabIndex={0}
+          ref={this.btnRef}
+          className={`${styles.button} ${formOpenClass} ${invertClass}`}
           onClick={this.toggleForm}
           type="button"
           aria-label="click to open contact form"
@@ -78,11 +183,11 @@ export default class Contact extends Component {
           >
             Contact
           </span>
+
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className={`${styles.btnIcon} ${formOpenClass}`}
             role="presentation"
-            ref={this.btnRef}
             aria-describedby="btndesc"
             viewBox="0 0 97.67 97.67"
           >
@@ -126,9 +231,52 @@ export default class Contact extends Component {
 
         <div
           ref={this.wrapperRef}
-          className={`${styles.formWrapper} ${styles.invertClass} ${formOpenClass}`}
-        >
-          <form className={styles.form}></form>
+          className={`${styles.formWrapper} ${invertClass} ${formOpenClass}`}
+        ></div>
+        <div ref={this.formRef} className={styles.form}>
+          {this.state.formVisible && (
+            <form className={styles.formContainer}>
+              <div className={styles.field}>
+                <label className={styles.label}>Name:</label>
+                <input
+                  ref={this.firstInputRef}
+                  name="name"
+                  className={styles.input}
+                  placeholder="Name"
+                />
+                <p>Exercitationem recusandae eaque inventore ratione.</p>
+              </div>
+
+              <div className={styles.field}>
+                <label className={styles.label}>Name:</label>
+                <input
+                  name="email"
+                  className={styles.input}
+                  placeholder="Email"
+                />{" "}
+                <p>Exercitationem recusandae eaque inventore ratione.</p>
+              </div>
+
+              <div className={styles.field}>
+                <label className={styles.label}>Name:</label>
+                <input
+                  name="subject"
+                  className={styles.input}
+                  placeholder="Subject"
+                />
+                <p>Exercitationem recusandae eaque inventore ratione.</p>
+              </div>
+              <div className={styles.field}>
+                <label className={styles.label}>Message</label>
+                <textarea
+                  ref={this.lastInputRef}
+                  name="message"
+                  placholder="message"
+                />{" "}
+                <p>Exercitationem recusandae eaque inventore ratione.</p>
+              </div>
+            </form>
+          )}
         </div>
       </div>
     )
