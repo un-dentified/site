0dddbd4d67a4ec10a9af6a96659331fc02e62d0a
shsangha <sssangha@ucalgary.ca>
2019-09-19 16:07:34 -0600

    fixed eslint violations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import React from "react"
 import PageWrapper from "../components/PageWrapper"
+import Seo from "../components/Seo"
 import AnimatedIndex from "../components/AnimatedIndex"
 
-export default props => (
-  <PageWrapper
-    {...props}
-    currentPage={""}
-    menuAccess={false}
-    menuOpen={true}
-    defaultLinks={["shop", "about", "music", "shows"]}
-  >
-    <AnimatedIndex />
-  </PageWrapper>
+const Index = props => (
+  <>
+    <Seo />
+    <PageWrapper
+      {...props}
+      currentPage=""
+      menuAccess={false}
+      menuOpen={true}
+      defaultLinks={["shop", "about", "music", "shows"]}
+    >
+      <AnimatedIndex />
+    </PageWrapper>
+  </>
 )
+
+export default Index
