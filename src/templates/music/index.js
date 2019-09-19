60e98aa87a55eee048a3cf9c3dc293b6314ad233
shsangha <sssangha@ucalgary.ca>
2019-09-19 16:09:53 -0600

    moved music into a template for pagination

--- src/templates/music/index.js
+++ src/templates/music/index.js
@@ -1,9 +1,16 @@
-import React, { Component } from "react"
+import React, { Component, createRef } from "react"
+import { graphql } from "gatsby"
+import PropTypes from "prop-types"
+import fade from "../../utils/fade"
 import PageWrapper from "../../components/PageWrapper"
 import AudioPreview from "../../components/AudioPreview"
+import Seo from "../../components/Seo"
 import styles from "./style.module.scss"
+import Link from "gatsby-plugin-transition-link"
 
 export default class MusicPage extends Component {
+  contentRef = createRef()
+
   state = {
     playing: "",
   }
@@ -13,19 +20,225 @@ export default class MusicPage extends Component {
   }
 
   render() {
-    const { props } = this
+    const {
+      data: {
+        allMarkdownRemark: { edges },
+      },
+      pageContext: { currentPage, numPages },
+      path,
+    } = this.props
+
+    const nextPath =
+      currentPage < numPages - 1 ? `/music/${currentPage + 1}` : path
+    const prevPath =
+      currentPage > 0
+        ? currentPage - 1 > 0
+          ? `/music/${currentPage - 1}`
+          : `/music`
+        : path
 
     return (
-      <PageWrapper
-        {...props}
-        currentPage={"music"}
-        menuAccess={true}
-        invertLinkColor
-        menuOpen={true}
-        defaultLinks={["", "about", "shop", "shows"]}
-      >
-        <div>/</div>
-      </PageWrapper>
+      <>
+        <Seo />
+        <PageWrapper
+          {...this.props}
+          currentPage="music"
+          menuAccess={true}
+          invertLinkColor
+          menuOpen={true}
+          defaultLinks={["", "about", "shop", "shows"]}
+        >
+          <div ref={this.contentRef} className={styles.musicPage}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="126"
+              className={styles.header}
+              height="49"
+              viewBox="0 0 126 49"
+            >
+              <g>
+                <path
+                  d="M30.67,38.7h-3.8L19.8,16.6V49H12.5V1h9.09l7.18,22.67L36,1H45V49h-7.3V16.6Z"
+                  transform="translate(-12 -0.5)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M72,45.08,68.41,49H53.69l-3.57-3.92V1h7.31V40.35l.8.88h5.64l.8-.88V1H72Z"
+                  transform="translate(-12 -0.5)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M91.25,14.83V9.78l-.86-.95H84.81L84,9.78v10l.86.95H95l3.57,3.91V45.08L95,49H80.21l-3.57-3.92V35.17H84v5.11l.86.95h5.58l.86-.95V29.42l-.86-1H80.21l-3.57-3.91V4.92L80.21,1H95l3.57,3.92v9.91Z"
+                  transform="translate(-12 -0.5)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M103.28,49V1h7.3V49Z"
+                  transform="translate(-12 -0.5)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M115.65,45.08V4.92L119.22,1h14.72l3.56,3.92v9.91h-7.3V9.78l-.86-.95h-5.52l-.87.95V40.22l.87,1h5.52l.86-1v-5h7.3v9.91L133.94,49H119.22Z"
+                  transform="translate(-12 -0.5)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeMiterlimit="10"
+                />
+              </g>
+            </svg>
+
+            <div className={styles.content}>
+              {edges.map(edge => {
+                const {
+                  node: { id, frontmatter },
+                } = edge
+
+                return (
+                  <AudioPreview
+                    key={id}
+                    id={id}
+                    {...frontmatter}
+                    playing={this.state.playing}
+                    setPlaying={this.setPlaying}
+                  />
+                )
+              })}
+            </div>
+            <div className={styles.navigationContainer}>
+              {currentPage > 0 && (
+                <Link
+                  exit={{
+                    length: 0.5,
+                    trigger: ({ exit, node }) =>
+                      fade({ exit, node, direction: "out" }),
+                  }}
+                  entry={{
+                    length: 0,
+                    trigger: ({ exit, node }) =>
+                      fade({ exit, node, direction: "in" }),
+                  }}
+                  className={styles.paginationLink}
+                  to={prevPath}
+                >
+                  <svg
+                    id="Layer_1"
+                    data-name="Layer 1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 94.15 94.15"
+                  >
+                    <polygon
+                      className={styles.svgLink}
+                      points="2.87 47.07 92.87 2.07 92.87 92.07 2.87 47.07"
+                      fill="#fff"
+                      stroke="#231f20"
+                      strokeMiterlimit="10"
+                      strokeWidth="2.56"
+                    />
+                  </svg>
+                </Link>
+              )}
+              {currentPage < numPages - 1 && (
+                <Link
+                  exit={{
+                    length: 0.5,
+                    trigger: ({ exit, node }) =>
+                      fade({ exit, node, direction: "out" }),
+                  }}
+                  entry={{
+                    length: 0,
+                    trigger: ({ exit, node }) =>
+                      fade({ exit, node, direction: "in" }),
+                  }}
+                  className={styles.paginationLink}
+                  to={nextPath}
+                >
+                  <svg
+                    id="Layer_1"
+                    data-name="Layer 1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 94.15 94.15"
+                  >
+                    <polygon
+                      className={styles.svgLink}
+                      points="91.28 47.07 1.28 92.07 1.28 2.07 91.28 47.07"
+                      fill="#fff"
+                      stroke="#231f20"
+                      strokeMiterlimit="10"
+                      strokeWidth="2.56"
+                    />
+                  </svg>
+                </Link>
+              )}
+            </div>
+          </div>
+        </PageWrapper>
+      </>
     )
   }
 }
+
+MusicPage.propTypes = {
+  path: PropTypes.string.isRequired,
+  pageContext: PropTypes.shape({
+    currentPage: PropTypes.number.isRequired,
+    numPages: PropTypes.number.isRequired,
+  }),
+  data: {
+    allMarkdownRemark: PropTypes.arrayOf(
+      PropTypes.shape({
+        node: {
+          id: PropTypes.string.isRequired,
+          frontmatter: PropTypes.shape({
+            Apple: PropTypes.string.isRequired,
+            Spotify: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            preview: PropTypes.string.isRequired,
+            cover: {
+              childImageSharp: {
+                fluid: PropTypes.object.isRequired,
+              },
+            },
+          }),
+        },
+      })
+    ),
+  },
+}
+
+export const query = graphql`
+  query Music($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      filter: { frontmatter: { Type: { eq: "music" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            Apple
+            Spotify
+            preview
+            title
+            cover {
+              childImageSharp {
+                fluid(maxWidth: 500) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/shows/index.js
@@ -0,0 +1,245 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import Link from "gatsby-plugin-transition-link"
+import Seo from "../../components/Seo"
+import fade from "../../utils/fade"
+import PageWrapper from "../../components/PageWrapper"
+import styles from "./style.module.scss"
+
+const Shows = props => {
+  const {
+    data: {
+      shows: { edges },
+    },
+    pageContext: { currentPage, numPages },
+    path,
+  } = props
+
+  const nextPath =
+    currentPage < numPages - 1 ? `/shows/${currentPage + 1}` : path
+  const prevPath =
+    currentPage > 0
+      ? currentPage - 1 > 0
+        ? `/shows/${currentPage - 1}`
+        : `/shows`
+      : path
+
+  return (
+    <>
+      <Seo />
+      <PageWrapper
+        {...props}
+        currentPage="shows"
+        menuAccess={true}
+        menuOpen={true}
+        defaultLinks={["", "about", "music", "shop"]}
+      >
+        <div className={styles.showsPage}>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={styles.header}
+              viewBox="0 0 83.86 36"
+            >
+              <title>Untitled-1</title>
+              <g>
+                <path
+                  d="M9.14,11.09V7.4l-.48-.69H5.55l-.48.69v7.28l.48.69h5.71l2,2.85V33.14l-2,2.86H3L1,33.14V25.92H5.07v3.72l.48.7H8.66l.48-.7V21.72L8.66,21H3L1,18.18V3.86L3,1h8.27l2,2.86v7.23Z"
+                  transform="translate(-0.5 -0.5)"
+                  fill="none"
+                  stroke="#231f20"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M19.93,1V15.46h4.58V1h4.07V36H24.51V21.12H19.93V36H15.85V1Z"
+                  transform="translate(-0.5 -0.5)"
+                  fill="none"
+                  stroke="#231f20"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M43.6,3.86V33.14l-2,2.86H33.4l-2-2.86V3.86L33.4,1h8.21ZM39,6.71H35.93l-.45.65V29.69l.45.65H39l.48-.65V7.36Z"
+                  transform="translate(-0.5 -0.5)"
+                  fill="none"
+                  stroke="#231f20"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M65.38,36H60.25L57.71,12,55.18,36H50.05L45.4,1h4.14l3,25.28L55.47,1H60l2.92,25.28L65.89,1H70Z"
+                  transform="translate(-0.5 -0.5)"
+                  fill="none"
+                  stroke="#231f20"
+                  strokeMiterlimit="10"
+                />
+                <path
+                  d="M79.76,11.09V7.4l-.48-.69H76.17l-.49.69v7.28l.49.69h5.7l2,2.85V33.14l-2,2.86H73.6l-2-2.86V25.92h4.07v3.72l.49.7h3.11l.48-.7V21.72L79.28,21H73.6l-2-2.85V3.86L73.6,1h8.27l2,2.86v7.23Z"
+                  transform="translate(-0.5 -0.5)"
+                  fill="none"
+                  stroke="#231f20"
+                  strokeMiterlimit="10"
+                />
+              </g>
+            </svg>
+          </div>
+          <ul className={styles.showList}>
+            {edges.map(edge => {
+              const {
+                node: {
+                  id,
+                  frontmatter: {
+                    link,
+                    title,
+                    date,
+                    image: {
+                      childImageSharp: { fluid },
+                    },
+                  },
+                },
+              } = edge
+              return (
+                <li className={styles.show} key={id}>
+                  <div className={styles.description}>
+                    <h3>{title}</h3>
+                    <span>{date}</span>
+                    <a className={styles.link} href={link}>
+                      Tickets
+                    </a>
+                  </div>
+                  <Img className={styles.image} fluid={fluid} />
+                </li>
+              )
+            })}
+          </ul>
+          <div className={styles.navigationContainer}>
+            {currentPage > 0 && (
+              <Link
+                exit={{
+                  length: 0.5,
+                  trigger: ({ exit, node }) =>
+                    fade({ exit, node, direction: "out" }),
+                }}
+                entry={{
+                  length: 0,
+                  trigger: ({ exit, node }) =>
+                    fade({ exit, node, direction: "in" }),
+                }}
+                className={styles.paginationLink}
+                to={prevPath}
+              >
+                <svg
+                  id="Layer_1"
+                  data-name="Layer 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 94.15 94.15"
+                >
+                  <polygon
+                    className={styles.svgLink}
+                    points="2.87 47.07 92.87 2.07 92.87 92.07 2.87 47.07"
+                    fill="#fff"
+                    stroke="#231f20"
+                    strokeMiterlimit="10"
+                    strokeWidth="2.56"
+                  />
+                </svg>
+              </Link>
+            )}
+            {currentPage < numPages - 1 && (
+              <Link
+                exit={{
+                  length: 0.5,
+                  trigger: ({ exit, node }) =>
+                    fade({ exit, node, direction: "out" }),
+                }}
+                entry={{
+                  length: 0,
+                  trigger: ({ exit, node }) =>
+                    fade({ exit, node, direction: "in" }),
+                }}
+                className={styles.paginationLink}
+                to={nextPath}
+              >
+                <svg
+                  id="Layer_1"
+                  data-name="Layer 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 94.15 94.15"
+                >
+                  <polygon
+                    className={styles.svgLink}
+                    points="91.28 47.07 1.28 92.07 1.28 2.07 91.28 47.07"
+                    fill="#fff"
+                    stroke="#231f20"
+                    strokeMiterlimit="10"
+                    strokeWidth="2.56"
+                  />
+                </svg>
+              </Link>
+            )}
+          </div>
+        </div>
+      </PageWrapper>
+    </>
+  )
+}
+
+Shows.propTypes = {
+  path: PropTypes.string.isRequired,
+  pageContext: {
+    currentPage: PropTypes.number.isRequired,
+    numPages: PropTypes.number.isRequired,
+  },
+  data: {
+    shows: {
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: {
+            id: PropTypes.string.isRequired,
+            frontmatter: {
+              title: PropTypes.string.isRequired,
+              date: PropTypes.string.isRequired,
+              image: {
+                childImageSharp: {
+                  fluid: PropTypes.object.isRequired,
+                },
+              },
+            },
+          },
+        })
+      ),
+    },
+  },
+}
+
+export default Shows
+
+export const query = graphql`
+  query Shows($skip: Int!, $limit: Int!) {
+    shows: allMarkdownRemark(
+      filter: { frontmatter: { Type: { eq: "shows" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            link
+            date
+            Type
+            image {
+              childImageSharp {
+                fluid(maxWidth: 900) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
