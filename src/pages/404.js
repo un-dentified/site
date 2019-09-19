97ef4485490ab7d324c1690cc0ba7259bfb0c6fb
shsangha <sssangha@ucalgary.ca>
2019-09-19 17:18:31 -0600

    altered 404 into an actual page instead of just redirecting

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,28 @@
-import { navigate } from "gatsby"
+import React from "react"
+import Link from "gatsby-plugin-transition-link"
+import fade from "../utils/fade"
+import styles from "./styles/404.module.scss"
 
 const FourOhFour = () => {
-  if (typeof window !== undefined) {
-    navigate("/")
-  }
-
-  return null
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.header}>Page not found</h2>
+      <Link
+        exit={{
+          length: 0.5,
+          trigger: ({ exit, node }) => fade({ exit, node, direction: "out" }),
+        }}
+        entry={{
+          length: 0,
+          trigger: ({ exit, node }) => fade({ exit, node, direction: "in" }),
+        }}
+        className={styles.link}
+        to="/"
+      >
+        Return Home
+      </Link>
+    </div>
+  )
 }
 
 export default FourOhFour
