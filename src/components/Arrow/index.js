8b65e5782e6c4b9daaf53c3c6a70c01dcb034b0f
shsangha <sssangha@ucalgary.ca>
2019-09-19 15:55:54 -0600

    added role presentation and removed title so that no tooltip appears over arrow (not needed kinda annoying)

--- src/components/Arrow/index.js
+++ src/components/Arrow/index.js
@@ -1,17 +1,16 @@
 import React from "react"
+import Proptypes from "prop-types"
 
-export default ({ className }) => {
+const Arrow = ({ className }) => {
   return (
     <svg
-      id="Layer_1"
-      data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
       width="28.39"
       height="29.49"
       viewBox="0 0 28.39 29.49"
       className={className}
+      role="presentation"
     >
-      <title>svg arrow</title>
       <polygon
         points="27.29 18.61 16.61 18.61 16.91 28.93 11.48 28.93 11.19 18.61 1.1 18.61 14.18 0.93 27.29 18.61"
         fill="#fff"
@@ -22,3 +21,9 @@ export default ({ className }) => {
     </svg>
   )
 }
+
+Arrow.propTypes = {
+  className: Proptypes.string.isRequired,
+}
+
+export default Arrow
